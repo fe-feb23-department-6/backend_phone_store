cf8d8eb49cfc4c2805cf005a314fef75f83da5e1
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-06-08 16:14:40 +0300

    initial beck end

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server is working on http://localhost:${PORT}`);
+});
