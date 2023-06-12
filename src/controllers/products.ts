989a4ad2403ac1e8e764e7ad1315030e117a2d62
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-06-12 18:48:15 +0300

    separate logic of controller and service

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,34 +1,20 @@
 'use strict';
 import { Request as Req, Response as Res } from 'express';
-
-import { Products } from '../models/Products';
-import { getProductsWitUrl } from '../utils/getProductsWitUrl';
-import { filterProducts } from '../utils/filterProdacts';
+import { productsService } from '../services/products';
 
 const getProducts = async(req: Req, res: Res) => {
   const { page = '1', limit = '16', sort = 'newest' } = req.query;
   const pageNumber = Number(page);
   const limitNumber = Number(limit);
-  const offset = (pageNumber - 1) * limitNumber;
-
-  const filterOption = filterProducts(sort);
 
   try {
-    const products = await Products.findAndCountAll({
-      order: filterOption,
-      offset: offset,
-      limit: limitNumber,
-    });
-
-    const totalPages = Math.ceil(products.count / limitNumber);
-
-    const productsWithURL = getProductsWitUrl(products.rows);
-
-    res.json({
-      products: productsWithURL,
-      currentPage: pageNumber,
-      totalPages: totalPages,
-    })
+    const paginationData = await productsService.getProductsWithPagination(
+      pageNumber, 
+      limitNumber, 
+      sort
+    );
+    
+    res.json(paginationData);
   } catch (error) {
     res.status(500);
   }
--- src/services/products.ts
+++ src/services/products.ts
@@ -1,9 +1,33 @@
 import { Products } from '../models/Products';
+import { filterProducts } from '../utils/filterProdacts';
+import { getProductsWitUrl } from '../utils/getProductsWitUrl';
+import { ParsedQs } from 'qs';
 
-function getAll() {
-  return Products.findAll();
-}
+const getProductsWithPagination = async (pageNumber: number, limitNumber: number, sort: string | ParsedQs | string[] | ParsedQs[]) => {
+  const offset = (pageNumber - 1) * limitNumber;
+  const filterOption = filterProducts(sort);
+
+  try {
+    const products = await Products.findAndCountAll({
+      order: filterOption,
+      offset: offset,
+      limit: limitNumber,
+    });
+
+    const totalPages = Math.ceil(products.count / limitNumber);
+
+    const productsWithURL = getProductsWitUrl(products.rows);
+
+    return {
+      products: productsWithURL,
+      currentPage: pageNumber,
+      totalPages: totalPages,
+    };
+  } catch (error) {
+    throw new Error('Failed to get products');
+  }
+};
 
 export const productsService = {
-  getAll,
+  getProductsWithPagination,
 };
