730a481945fda27494352e274c46b1e3c5f08d23
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-06-19 05:39:47 +0300

    fixed products endpoints

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -6,11 +6,11 @@ import { productsController } from '../controllers/products';
 export const router = express.Router();
 
 router.get('/', productsController.getProducts);
-router.get('/:productId', productsController.getOneProductById);
 router.get('/phones', productsController.getProducts);
+router.get('/phones/:phoneId', productsController.getNamespaceListByProductsId);
 router.get('/tablets', productsController.getProducts);
 router.get('/accessories', productsController.getProducts);
 router.get('/new', productsController.getNewestProducts);
 router.get('/discount', productsController.getHotPriceProducts);
-router.get('/:namespaceId', productsController.getProductsById);
+router.get('/:productId', productsController.getOneProductById);
 router.get('/:namespaceId/recommended', productsController.getRecommended);
